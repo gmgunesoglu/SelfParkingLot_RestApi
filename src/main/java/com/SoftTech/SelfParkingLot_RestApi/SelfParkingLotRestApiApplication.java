ed882e1c212ace81cbb8b9623960323825bbb198
gokhanmuzaffer <gokhangunesoglu@gmail.com>
2023-07-30 01:09:35 +0300

    Otomatik Otopark Sistemi SoftTech Staj Donemi Java Spring Boot Postgresql RestApi

---
+++ src/main/java/com/SoftTech/SelfParkingLot_RestApi/SelfParkingLotRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.SoftTech.SelfParkingLot_RestApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfParkingLotRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelfParkingLotRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SoftTech/SelfParkingLot_RestApi/SelfParkingLotRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SoftTech.SelfParkingLot_RestApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfParkingLotRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
